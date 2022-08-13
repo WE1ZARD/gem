34236d2e3aa9287a46e1513af747b800815d6109
WEIZARD <111175406+WE1ZARD@users.noreply.github.com>
2022-08-13 17:53:48 +0800

    Create gem.lua

---
+++ gem.lua
@@ -0,0 +1,49 @@
+--[[
+============================
+制作/使用 法力宝石
+
+1. 制作宝石的宏命令/代码命令 "/conjuregem" or /run ConjureGem() 
+2. 使用宝石的宏命令/代码命令 "/usegem" or /run UseGem()
+============================
+]]
+
+local gem = {"法力红宝石","法力黄水晶","法力翡翠","法力玛瑙"}
+local function checkGem(gem) 
+    local f,v
+    for i = 0,4 do 
+        for k = 1,GetContainerNumSlots(i) do 
+            v = GetContainerItemLink(i,k) or ""
+            if strfind(v,gem) then f = 1 end
+        end
+    end
+    if not f then CastSpellByName("制造"..gem) end
+end
+
+function ConjureGem()
+    for _,v in ipairs (gem) do
+        checkGem(v)
+    end
+end
+
+SLASH_CONJUREGEM1 = "/conjuregem"
+SlashCmdList["CONJUREGEM"] = function(arg)ConjureGem()end
+
+local function FindGem(gem)
+    for b = 0,4 do 
+        for s = 1,GetContainerNumSlots(b,s)do 
+            local n = GetContainerItemLink(b,s)
+            if n and strfind(n,gem) then 
+                UseContainerItem(b,s)
+            end 
+        end
+    end
+end
+
+function UseGem()
+    for _,v in ipairs(gem) do
+        FindGem(v)
+    end
+end
+
+SLASH_USEGEM1 = "/usegem"
+SlashCmdList["USEGEM"] = function(arg)UseGem()end
